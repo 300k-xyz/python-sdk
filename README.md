76d383d26296f6aaa90a627fdc2ef669aa149b00
wanghao <whateverhow@gmail.com>
2023-05-16 10:46:17 -0500

    [master]   (use "git restore --staged <file>..." to unstage)

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
-# py-sliding-window
+# 300k-python-sdk
 
-pip install git+https://github.com/300k-xyz/python-sdk.git#main
+pip install git+https://github.com/300k-xyz/python-sdk.git#egg=tkpysdk
  
  ### usage
 ```
 from tkpysdk import create_300k_header
 
-create_300k_header("POST", path = "/api/v1/smoke", api_key = "x", api_secret = "y", post_data: {})
+create_300k_header("POST", path = "/api/v1/smoke", api_key = "x", api_secret = "y", post_data = {})
+
+# output:
+# {'X-APIKEY': 'x', 'X-TS': 1684251958440, 'X-SIGNATURE': '4fab963b64bc9a89e979596a8714db7cdb3bf79d806b6fb146ecf62f1632b5ea'}
+
 ```
--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-import os
 import setuptools
 
 setuptools.setup(
@@ -8,5 +7,24 @@ setuptools.setup(
     author="Hao W",
     license="Copyright 2023, 300k ltd, All rights reserved",
     package_dir={'': 'src'},
-    packages=setuptools.find_packages(where='src')
+    packages=setuptools.find_packages(where='src'),
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3',
+    ]
+
 )
